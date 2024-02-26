4c762be545470c7ee33cc43f980bfb11be152d6b
dz2244 <dz2244@bs.amalnet.k12.il>
2024-02-26 15:12:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/exc0610/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.exc0610;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout wp;
+    Button btn;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
